35fa1868f65bf9f1534d79f1fcf260f62b07fdd3
yossi78 <yossi78@gmail.com>
2024-08-27 13:05:04 +0300

    First commit

---
+++ src/main/java/com/playermaker/com/players_analyzer/PlayersAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.playermaker.com.players_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playermaker/com/players_analyzer/PlayersAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playermaker.com.players_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
