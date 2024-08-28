717df995f7a991d50382f030cb12a69b317e8fff
yossi78 <yossi78@gmail.com>
2024-08-28 11:07:08 +0300

    Add more unit tests

--- src/test/java/com/playermaker/com/players_analyzer/service/PlayerServiceTests.java
+++ src/test/java/com/playermaker/com/players_analyzer/service/PlayerServiceTests.java
@@ -1,13 +1,11 @@
 package com.playermaker.com.players_analyzer.service;
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class PlayerServiceTests {
@@ -26,4 +24,33 @@ class PlayerServiceTests {
         List<String> topPlayers = playerService.getTopPlayers(2, participatedPlayers);
         assertEquals(Arrays.asList("Ronaldo", "Shalom"), topPlayers);
     }
+
+    @Test
+    void testGetTopPlayersWithNullParticipatedPlayers() {
+        assertThrows(NullPointerException.class, () -> {
+            playerService.getTopPlayers(3, null);
+        });
+    }
+
+
+    @Test
+    void testGetTopPlayersWithEmptyGameList() {
+        List<List<String>> participatedPlayers = Collections.emptyList();
+        List<String> result = playerService.getTopPlayers(3, participatedPlayers);
+        assert result.isEmpty();
+    }
+
+
+    @Test
+    void testGetTopPlayersWithEmptyPlayersInGame() {
+        List<List<String>> participatedPlayers = Arrays.asList(
+                Arrays.asList(),
+                Arrays.asList("Messi", "Ronaldo", "Shalom")
+        );
+        List<String> result = playerService.getTopPlayers(2, participatedPlayers);
+        assert result.size() == 2;
+        assert result.contains("Ronaldo");
+        assert result.contains("Shalom");
+    }
 }
+
